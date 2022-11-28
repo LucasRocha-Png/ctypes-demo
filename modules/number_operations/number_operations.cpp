fe7be48b9d0f0cf23a405460086ef9203362af67
Lucas Rocha <44853524+SbbjBr@users.noreply.github.com>
2022-11-28 15:36:29 -0300

    Create number operations functions using c++

---
+++ modules/number_operations/number_operations.cpp
@@ -0,0 +1,17 @@
+extern "C" {
+#include "number_operations.h"
+
+int add_two_int(int a, int b){
+	return a + b;
+}
+
+
+float add_two_float(float a, float b){
+	return a + b;
+}
+
+
+double add_two_double(double a, double b){
+	return a + b;
+}
+}
---
+++ modules/number_operations/number_operations.h
@@ -0,0 +1,9 @@
+extern "C" {
+
+int add_two_int(int a, int b);
+
+float add_two_float(float a, float b);
+
+double add_two_double(double a, double b);
+
+}
