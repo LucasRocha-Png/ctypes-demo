18200cda8c80c2b19eb06744ec5a0b9ae294770e
Lucas Rocha <lucasrocha.20166@gmail.com>
2022-12-19 09:01:16 -0300

    Coments cmake

--- modules/cmake_operations.py
+++ modules/cmake_operations.py
@@ -1,3 +1,14 @@
+"""
+This script imports the "cmake_operations.dll" file, compilled with the command 
+"cmake .." "cmake --build ." at cmake_operations/build/ folder,
+and them, create a function that calls the C++ functions.
+To create a .DLL or .SO (depending of your operating system) file you must have CMake.
+
+This file declares the following functions:
+hello
+"""
+
+
 import ctypes
 import os
 
@@ -6,5 +17,6 @@ archive_folder = os.path.dirname(__file__)
 
 library = ctypes.CDLL(f"{archive_folder}\\cmake_operations\\build\\Debug\\cmake-tests.dll")
 
+#Print Hello
 library.hello()
 
